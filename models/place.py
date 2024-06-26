cc1d9e2d0806ab93eaa033a4d296d981f9591ddf
7gaar <m3gamind10@gmail.com>
2024-06-27 01:03:26 +0300

    task 9

--- models/place.py
+++ models/place.py
@@ -3,7 +3,9 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, Integer, ForeignKey, Float, Table
+from sqlalchemy.orm import relationship
 from os import getenv
+from models import storage, Review
 
 storage_type = getenv('HBNB_TYPE_STORAGE')
 
@@ -22,6 +24,8 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer, nullable=False, default=0)
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
+        reviews = relationship('Review', backref='place',
+                               cascade='all, delete, delete-orphan')
     else:
         city_id = ""
         user_id = ""
@@ -34,3 +38,12 @@ class Place(BaseModel, Base):
         latitude = 0.0
         longitude = 0.0
         amenity_ids = []
+
+        @property
+        def reviews(self):
+            """Returns list of reviews.id"""
+            revs = []
+            for rev in list(storage.all(Review).values):
+                if rev.place_id == self.id:
+                    revs.append(rev)
+            return revs
